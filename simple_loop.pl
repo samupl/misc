5dfb4fc89b59887320581b186b691b1b0f5e9bf0
Jakub Szafrański <s@samu.pl>
2011-06-23 18:51:34 +0200

    simple_loop

---
+++ simple_loop.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+#
+# A 'simple' script which should write numbers from 1 to 20.
+# On IRC 'diamondcat' asked for such a simple code, so here it is.
+#
+# $ perl -v
+#
+# This is perl 5, version 12, subversion 3 (v5.12.3) built for i686-linux-thread-multi
+#
+# Dont ask why. I am not going to debug that. No fuckin' wai.
+#
+# Author: samu (#main @ pirc.pl)
+#
+;my@a=qw();nothing;my$x=1;my@y=1;'@my$y""@';'my$z';nothing;my@x=qw($z,x,y,z,@a;;nothing;foreach($_0{#2_;_1&&,;;nothing;usleep+666x;@s(@@x%)@for$_0;;nothing;eval(;@a=$x(#;)s/@a=~/^[0-9]+/i;@(eval{$x@a(usleep+e21-1;;while$_()$x++;;sqrt(\@a[0]={$_;}++;)foreach(@a){;$x++;$z=$x+@a[$_];;for($x;$"z"ne"$x";++$x){$x=$z+$_-@a[1];;};usleep+1e6;@a[$_]->push(@x,$x);;$z++;@a[$_]++;while$_@a[][]++;nothing;;return0;else{return @_[][];;@x=qw($a)++;$_->push();}while$_()$x++;;sqrt(\@a[0]={$_;}++;){#;;@a[][]++$x=~s///;}$x=$z+$_-@a[1];;}})}))}));;for($x;"$x"ne"21";$x+@y){push(@a,$x);;usleep+1e6;$x+=(@y);;usleep+1e5};;;foreach(@a){;print$z;$z="$_\n";;my$_;;my@n=qw($_);};print$z
